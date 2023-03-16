ac02ee55e047319578b7981e62b9f8d168178654
Николай Степанов <stepanovnic.03@yandex.ru>
2023-03-16 19:50:58 +0300

    improve codecov and delete try-catch

--- project/src/Top.cpp
+++ project/src/Top.cpp
@@ -28,21 +28,23 @@ int Top::getFilmsInfo() {
     if (!basics.is_open()) {
         return 1;
     }
-    try {
-        std::string line;
-        while (std::getline(basics, line)) {
-            std::string id, type, title, originalTitle, startYear, isAdult;
-            std::istringstream ss(line);
-            ss >> id >> std::ws >> type >> std::ws;
-            std::getline(ss, title, '\t');
-            std::getline(ss, originalTitle, '\t');
-            ss >> isAdult >> std::ws >> startYear >> std::ws;
-            if (type == "movie" && startYear == year && isAdult == "0") {
-                films.emplace_back(id, -1, title);
-            }
+    std::string line;
+    bool flag = false;
+    while (std::getline(basics, line)) {
+        std::string id, type, title, originalTitle, startYear, isAdult;
+        std::istringstream ss(line);
+        std::getline(ss, id, '\t');
+        std::getline(ss, type, '\t');
+        std::getline(ss, title, '\t');
+        std::getline(ss, originalTitle, '\t');
+        std::getline(ss, isAdult, '\t');
+        std::getline(ss, startYear, '\t');
+        if ((isAdult.empty() || startYear.empty() || type.empty()) && flag)
+            return 1;
+        if (type == "movie" && startYear == year && isAdult == "0") {
+            films.emplace_back(id, -1, title);
         }
-    } catch (...) {
-        return 1;
+        flag = true;
     }
     return 0;
 }
@@ -52,25 +54,26 @@ int Top::getFilmsRates() {
     if (!ratings.is_open()) {
         return 1;
     }
-    try {
-        std::string line;
-        int i = 0;
-        while (std::getline(ratings, line)) {
-            std::string id;
-            float rate;
-            int numVotes;
-            std::istringstream ss(line);
-            ss >> id >> std::ws >> rate >> numVotes;
-            if (id == films[i].id) {
-                if (numVotes >= 1000)
-                    films[i].rate = rate;
-                i++;
-            }
+    std::string line;
+    int i = 0;
+    bool flag = false;
+    while (std::getline(ratings, line)) {
+        std::string id;
+        float rate = -1;
+        int numVotes = -1;
+        std::istringstream ss(line);
+        std::getline(ss, id, '\t');
+        ss >> rate >> numVotes;
+        if ((rate == -1 || numVotes == -1) && flag)
+            return 1;
+        if (id == films[i].id) {
+            if (numVotes >= 1000)
+                films[i].rate = rate;
+            i++;
         }
-        std::erase_if(films, [](auto film) { return film.rate == -1;});
-    } catch(...) {
-        return 1;
+        flag = true;
     }
+    std::erase_if(films, [](auto film) { return film.rate == -1;});
     return 0;
 }
 
@@ -79,27 +82,28 @@ int Top::getRuNames() {
     if (!akas.is_open()) {
         return 1;
     }
-    try {
-        std::string line;
-        int i = 0;
-        while (std::getline(akas, line)) {
-            std::string id, title, region;
-            int ordering;
-            std::istringstream ss(line);
-            ss >> id >> std::ws >> ordering >> std::ws;
-            std::getline(ss, title, '\t');
-            ss >> region;
-            if (id > films[i].id && i + 1 < films.size())
+    std::string line;
+    int i = 0;
+    bool flag = false;
+    while (std::getline(akas, line)) {
+        std::string id, title, region;
+        int ordering;
+        std::istringstream ss(line);
+        std::getline(ss, id, '\t');
+        ss >> ordering >> std::ws;
+        std::getline(ss, title, '\t');
+        std::getline(ss, region, '\t');
+        if ((region.empty() || id.empty() || title.empty()) && flag)
+            return 1;
+        if (id > films[i].id && i + 1 < films.size())
+            i++;
+        if (id == films[i].id) {
+            if (region == "RU") {
+                films[i].name = title;
                 i++;
-            if (id == films[i].id) {
-                if (region == "RU") {
-                    films[i].name = title;
-                    i++;
-                }
             }
         }
-    } catch (...) {
-        return 1;
+        flag = true;
     }
     return 0;
 }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,8 +1,16 @@
 #include <gtest/gtest.h>
 #include <vector>
+#include <fstream>
 
 #include "Film.h"
 #include "Top.h"
+
+void generateFailData() {
+    std::ofstream out("../../data/fail.tsv");
+    out << "titleId\tordering\ttitle\tregion\tlanguage\ttypes\tattributes\tisOriginalTitle\n"
+           "tt0000000 1 Карменсіта UA \\N imdbDisplay \\N 0\n"
+           "tt0000001 2 Carmencita DE \\N \\N literal title 0\n";
+}
 
 TEST(FilmSuite, TestFilmInit) {
   Film f("tt0000000", -1, "test_film");
@@ -19,7 +27,7 @@ TEST(FilmSuite, TestFilmOutput) {
     EXPECT_EQ(output, "test_film -1");
 }
 
-TEST(TopSuite, TestTop) {
+TEST(TopSuite, TestTopSimple) {
     Top top("2005", "../../data/title.akas.tsv",
             "../../data/title.basics.tsv", "../../data/title.ratings.tsv");
     int res = top.createTop();
@@ -30,6 +38,24 @@ TEST(TopSuite, TestTop) {
             {"t1", 7.6, "Звёздные войны. Эпизод 3: Месть Ситхов"},
             {"t2", 7.3, "Труп невесты"},
             {"t3", 5.7, "Фантастическая четвёрка"},
+    };
+    EXPECT_EQ(expected.size(), films.size());
+    for (int i = 0; i < films.size(); i++) {
+        EXPECT_EQ(films[i].name, expected[i].name);
+        EXPECT_EQ(films[i].rate, expected[i].rate);
+    }
+}
+
+TEST(TopSuite, TestTopEnglish) {
+    Top top("1987", "../../data/title.akas.tsv",
+            "../../data/title.basics.tsv", "../../data/title.ratings.tsv");
+    int res = top.createTop();
+    auto films = top.getTop();
+    EXPECT_EQ(res, 0);
+
+    std::vector<Film> expected = {
+            {"t1", 7.7, "Blind Chance"},
+            {"t2", 5.8, "Blood Rage"}
     };
     EXPECT_EQ(expected.size(), films.size());
     for (int i = 0; i < films.size(); i++) {
@@ -49,6 +75,24 @@ TEST(TopSuite, TestFailFileOpen) {
     EXPECT_EQ(res, 1);
     Top top3("2005", "../../data/title.akas.tsv", "../../data/title.basics.tsv",
              "");
+    res = top3.createTop();
+    EXPECT_EQ(res, 1);
+}
+
+TEST(TopSuite, TestWrongData) {
+
+    generateFailData();
+
+    Top top1("2005", "../../data/fail.tsv", "../../data/title.basics.tsv",
+             "../../data/title.ratings.tsv");
+    int res = top1.createTop();
+    EXPECT_EQ(res, 1);
+    Top top2("2005", "../../data/title.akas.tsv", "../../data/fail.tsv",
+             "../../data/title.ratings.tsv");
+    res = top2.createTop();
+    EXPECT_EQ(res, 1);
+    Top top3("2005", "../../data/title.akas.tsv", "../../data/title.basics.tsv",
+             "../../data/fail.tsv");
     res = top3.createTop();
     EXPECT_EQ(res, 1);
 }
